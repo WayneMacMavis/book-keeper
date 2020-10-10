0e811e4a7b809ac91ce73de8b8a3f6d2b1e937ee
Wayne Mac Mavis <w.macmavis83@gmail.com>
2020-10-10 19:51:26 +0200

    localstorage

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarkContainer = document.getElementById('bookmark-container');
 
+let bookmarks = [];
+
 // Show Modal, Focus on Input
 function showModal () {
     modal.classList.add('show-modal');
@@ -17,6 +19,49 @@ modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
 
+// Validate Form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+const regex = new RegExp(expression);
+if (!nameValue || !urlValue) {
+    alert('Please submit values for both fields.');
+    return false;
+}
+
+if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+}
+// Valid
+return true;
+}
+
+// Build bookmarks DOM
+function buildBookmarks() {
+    // Build Items
+    bookmarks.forEach((bookmark) => {
+        
+    });
+}
+
+// Fetch Bookmarks
+function fetchBookmarks() {
+    // Get bookmarks from localStorage if available
+    if (localStorage.getItem('bookmarks')) {
+            bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        // Create bookmarks array in localStorage
+        bookmarks = [
+            {
+                name: 'Wayne Mac Mavis',
+                url: 'https://github.com/WayneMacMavis',
+            },
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    buildBookmarks();
+}
+
 // Handle Data From Form
 function storeBookmark (e) {
     e.preventDefault();
@@ -25,8 +70,22 @@ function storeBookmark (e) {
     if (!urlValue.includes('http://', 'https://')) {
         urlValue = `https://${urlValue}`;
     }
-    console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
 // Event Listener
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+// On Load, Fetch Bookmarks
+fetchBookmarks();
